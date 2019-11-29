d23eb722f88afcc0b657a18a289ca573d1bade0c
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-29 13:57:45 -0500

    formatting

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -2,25 +2,25 @@ export const SET_USER = "SET_USER";
 export const SET_RECIPES = "SET_RECIPES";
 export const SET_MEAL_LIST = "SET_MEAL_LIST"
 const appReducer = (state, action) => {
-    console.log(action)
+  console.log(action)
   switch (action.type) {
     case SET_USER:
-      const { user } = action;
-
+    const user = action.value;
+    console.log('reducer called')
       return {
         ...state,
         user
       };
-      case SET_MEAL_LIST:
-          return {
-              ...state,
-              mealList:[...state.mealList,...action.mealList]
-          }
-    /*  case SET_RECIPES: //this is the type
-            return {
-                ...state,
-                recipes:action.recipes
-            } */
+    case SET_MEAL_LIST:
+      return {
+        ...state,
+        mealList:[...state.mealList,...action.mealList]
+      }
+    /*case SET_RECIPES: //this is the type
+      return {
+          ...state,
+          recipes:action.recipes
+      } */
     default:
       return state;
   }
