640e77818e73e01a6763df426f093b4a0f69cd4c
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-29 18:58:22 -0500

    dedicated Meal Page

---
+++ src/pages/MealPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import MealPlanSection from "../components/MealPlan/MealPlanSection";
+import NavBar from "../components/partials/Navbar";
+import Footer from "../components/partials/Footer";
+import { Container } from "react-bootstrap";
+import ProfileInfo from "../components/Profile/ProfileInfoSection/ProfileInfo";
+const MealPlan = ({ user, dispatch, mealList }) => {
+  console.log('Dispatch',dispatch)
+  return (
+    <Container className="" fluid={true}>
+      <NavBar user={user} />
+   
+      <ProfileInfo user={user} />
+      <MealPlanSection user={user} dispatch={dispatch} mealList={mealList} />
+    
+      <Footer />
+    </Container>
+  );
+};
+
+export default MealPlan;
