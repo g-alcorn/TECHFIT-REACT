b01efb958b54e6a81923aece2630a60b530cc7a4
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-29 18:57:35 -0500

    formatting

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import Navbar from "../components/partials/Navbar";
 import Footer from "../components/partials/Footer";
-import MealPlanSection from "../components/MealPlan/MealPlanSection";
 import ProfileInfo from "../components/Profile/ProfileInfoSection/ProfileInfo";
-import FitnessPlanSection from "../components/FitnessPlan/FitnessPlanSection";
+import MealPlanSection from "../components/MealPlan/MealPlanSection";
 /* import Tracker from "../components/liquids/Tracker.js";
 import Chart from "../components/liquids/Chart.js"; */
 
-export default function Profile({ dispatch, user, workoutList }) {
+export default function Profile({ dispatch, user, mealList }) {
   return (
     <Container className="" fluid={true}>
       <Navbar user={user} />
@@ -16,7 +15,7 @@ export default function Profile({ dispatch, user, workoutList }) {
 
       <Row style={{ borderBottom: "1px solid black" }}>
         <Col lg={12}>
-          <FitnessPlanSection user={user} workoutList={workoutList} dispatch={dispatch} />
+          <MealPlanSection user={user} mealList={mealList} dispatch={dispatch} />
     
         </Col>
      
