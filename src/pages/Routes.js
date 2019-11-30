c66d35c571474bb732b704cd695d738db66d19bb
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-30 18:39:35 -0500

    conditional rendering fixed

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,8 +1,9 @@
 import React, { useReducer, useEffect } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./Home";
- import Profile from "./Profile";
-import MealPlan from './MealPlan'
+import Profile from "./Profile";
+import MealPage from './MealPage';
+import FitnessPage from './FitnessPage';
 import Login from "./Login";
 import Register from "./Register";
 import appReducer, { SET_USER } from "../reducers/appReducer";
@@ -45,41 +46,36 @@ const Routes = (props) => {
       })
   }, [state.login])
 
-
-  // Functions
-  // const checkAuth = () => {
-  //   if (localStorage.getItem("token")) {
-  //     console.log("token is true", localStorage.getItem("token"))
-  //     return true;
-  //   } else {
-  //     console.log("token is false",localStorage.getItem("token"))
-  //     return false;
-  //   }
-  // };
-  // // const auth = () => {
-  //   if (checkAuth()) {
-  //     console.log('redirect to /');
-  //     // window.location.href = "/";
-  //     // props.history.push('/')
-  //     return <Redirect to='/' />
-  //   }
-  // };
-
   // Render
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/home" component={() => <Home />} />
-        <Route path="/login" component={() => <Login login={state.login}  dispatch={dispatch} />} />
-        <Route path="/register" component={() => <Register />} />
+        <Route 
+          path="/home" 
+          component={() => 
+            state.user ? (<Redirect to={"/"}/>) : (<Home />)
+          } />
+        <Route 
+          path="/login" 
+          component={() => 
+            state.user ? (<Redirect to={"/"}/>) : (<Login login={state.login}  dispatch={dispatch} />)
+          } />
+        <Route 
+          path="/register" 
+          component={() => 
+            state.user ? (<Redirect to={"/"}/>) : (<Register />)} />
   
         <PrivateRoute
           path="/meal-plan"
-          component={() => <MealPlan dispatch={dispatch} login={state.login}  mealList={state.mealList} user={state.user} />}
+          component={() => <MealPage dispatch={dispatch} login={state.login}  mealList={state.mealList} user={state.user} />}
         />
         <PrivateRoute
           path="/"
-          component={() => <Profile userLoading={state.userLoading}  dispatch={dispatch} mealList={state.mealList} user={state.user} />}
+          component={() => <Profile userLoading={state.userLoading} dispatch={dispatch} mealList={state.mealList} user={state.user} />}
+        />
+        <PrivateRoute
+          path="/fitness-plan"
+          component={() => <FitnessPage userLoading={state.userLoading} dispatch={dispatch} mealList={state.mealList} user={state.user} />}
         />
       </Switch>
     </BrowserRouter>
