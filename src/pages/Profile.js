c38bfe253a2f66e59337170e6cd9b3626f14ffc7
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-29 19:03:32 -0500

    returned to chart view

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,26 +1,151 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Button, ButtonToolbar, Accordion, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Navbar from "../components/partials/Navbar";
 import Footer from "../components/partials/Footer";
-import ProfileInfo from "../components/Profile/ProfileInfoSection/ProfileInfo";
 import MealPlanSection from "../components/MealPlan/MealPlanSection";
-/* import Tracker from "../components/liquids/Tracker.js";
-import Chart from "../components/liquids/Chart.js"; */
+import ProfileInfo from "../components/Profile/ProfileInfoSection/ProfileInfo";
+import Tracker from "../components/liquids/Tracker.js";
+import LiquidBar from "../components/liquids/LiquidBar.js";
+import LiquidPie from "../components/liquids/LiquidPie.js";
+import Incrementer from "../components/liquids/Incrementer";
+import UserInfoForm from "../components/Profile/ProfileInfoSection/UserInfoForm";
 
-export default function Profile({ dispatch, user, mealList }) {
+
+export default function Profile({ auth, dispatch, user, mealList }) {
+  const imageStyle = { height: "100px", width: "100px", marginBottom: "40px" };
+
+  function handleIncrease(event) {
+    console.log(event);
+  }
+
+  function handleDecrease(event) {
+    console.log(event);
+  }
+
   return (
     <Container className="" fluid={true}>
       <Navbar user={user} />
+
       <ProfileInfo  user={user} />
+      <Row
+      style={{ marginTop: "100px", borderBottom: "1px solid black" }}
+      className="p-5 text-center"
+      >
+        <Col lg={2}>
+          <img className='rounded-circle' src={user.image_url} alt="Profile" style={imageStyle}></img>
+            <h3>{user.first_name} {user.last_name}</h3>
+        </Col>
+        <Col lg={4}>
+          <Accordion className='mt-2 mb-2'>
+            <Card style={{ backgroundColor: "#f3f3f3",borderRadius:'10px' }}>
+              <Card.Header>
+                <Accordion.Toggle
+                
+                
+                  eventKey="0"
+                  style={{ color: "black" }}
+                >
+              <span >Update User Info </span> <i className="far fa-address-card"></i>
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <UserInfoForm user={user} />
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Col>
+        <Col lg={3}></Col>
+        <Col lg={3} style={{ textAlign: "right" }}>
+          <ButtonToolbar className='mt-3'>
+            <Link
+              style={{ textDecoration: "none", color: "#000" }}
+              to="/meal-plan"
+            >
+              {" "}
+              <Button
+                variant="secondary"
+                size="sm"
+                style={{ marginRight: "15px", height: "50px", width: "100px" }}
+              >
+                Meal Plan
+              </Button>
+            </Link>
+            <Link
+              style={{ textDecoration: "none", color: "#000" }}
+              to="/fitness-plan"
+            >
+              {" "}
+              <Button
+                variant="outline-secondary"
+                size="sm"
+                style={{ marginRight: "15px", height: "50px", width: "100px" }}
+              >
+                Fitness Plan
+              </Button>
+            </Link>
+          </ButtonToolbar>
+        </Col>
+      </Row>
 
       <Row style={{ borderBottom: "1px solid black" }}>
         <Col lg={12}>
           <MealPlanSection user={user} mealList={mealList} dispatch={dispatch} />
-    
         </Col>
-     
       </Row>
-     
+
+      <Row style={{ borderBottom: "1px solid black" }}>
+        <Col lg={4} style={{ borderRight: "1px solid black" }}>
+          <Tracker
+            waterCounter={() =>
+            <Incrementer 
+              name={'Water '} 
+              user={user}
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+            />}
+            coffeeCounter={() =>
+            <Incrementer 
+              name={'Coffee'} 
+              user={user}
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+            />}
+            sodaCounter={() =>
+            <Incrementer 
+              name={'Soda '} 
+              user={user}
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+            />}
+            otherCounter={() =>
+            <Incrementer 
+              name={'Other '} 
+              user={user} 
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+            />}
+            />
+          </Col>
+          <Col lg={8}>
+            <h3 className="text-center" style={{ marginTop: "15px" }}>
+              {" "}
+              Liquid Consumption Chart
+            </h3>
+            <Row>
+            <Col>
+              <LiquidBar />
+            </Col>
+            
+            <Col>
+              <LiquidPie />
+            </Col>
+            </Row>
+          </Col>
+      </Row>
+
       <Footer />
     </Container>
   );
