73466ef163689b67a36f0659d6583fac1cad54ee
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-12-02 18:41:49 -0500

    unused vars removed

--- src/hooks/useDrinksTracking.js
+++ src/hooks/useDrinksTracking.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import axios from 'axios';
 import { SET_DRINK_COUNT } from "../reducers/appReducer";
 
 export default function useDrinksTracking() {
   function handleCountSave(dispatch, userId, drinkType, operation) {
-    console.log('handleCountSave custom hook');
     axios
       .post(`/api/user-drinks?id=${userId}&action=${operation}&drinkType=${drinkType}`)
       .then((res) => {
