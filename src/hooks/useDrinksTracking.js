3c738a2a5ad7f54d316708b7076b769aaba8eccc
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-12-03 10:36:40 -0500

    no longer sends data to server if count is 0

--- src/hooks/useDrinksTracking.js
+++ src/hooks/useDrinksTracking.js
@@ -2,19 +2,29 @@ import axios from 'axios';
 import { SET_DRINK_COUNT } from "../reducers/appReducer";
 
 export default function useDrinksTracking() {
-  function handleCountSave(dispatch, userId, drinkType, operation) {
-    axios
-      .post(`/api/user-drinks?id=${userId}&action=${operation}&drinkType=${drinkType}`)
-      .then((res) => {
-        dispatch({
-          type: SET_DRINK_COUNT, 
-          drinkType: drinkType + "Count",
-          operation
-        })
+  function handleCountSave(dispatch, userId, drinkType, operation, drinkCounts) {
+    const drinkCountStr = drinkType + "Count"
+
+    if (drinkCounts[drinkCountStr] === 0 && operation === 'decrease') {
+      dispatch({
+        type: SET_DRINK_COUNT,
+        drinkType: drinkCountStr,
+        operation
       })
-      .catch(e => {
-        console.log(e)
-      });
+    } else {
+      axios
+        .post(`/api/user-drinks?id=${userId}&action=${operation}&drinkType=${drinkType}`)
+        .then((res) => {
+          dispatch({
+            type: SET_DRINK_COUNT, 
+            drinkType: drinkCountStr,
+            operation
+          })
+        })
+        .catch(e => {
+          console.log(e)
+        });
+    }
   }
 
 
